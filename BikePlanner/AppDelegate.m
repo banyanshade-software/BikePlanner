8f81aada4a8a460c13a6da99a61e38958ac1a433
Daniel <braun.daniel@free.fr>
2025-08-10 13:57:05 +0200

    add street view untested url

--- BikePlanner/AppDelegate.m
+++ BikePlanner/AppDelegate.m
@@ -79,6 +79,8 @@
     [self.mapView addGestureRecognizer:clicker];
 
     NSString *s = @"https://www.google.com/maps/@44.1042884,0.5239076,3a,75y,166.01h,82.2t/data=!3m7!1e1!3m5!1sUAywig97gsSiSgRpXM4R_g!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D7.796108031354706%26panoid%3DUAywig97gsSiSgRpXM4R_g%26yaw%3D166.01471008172715!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI1MDgwNi4wIKXMDSoASAFQAw%3D%3D";
+    // https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=44.1042884,0.5239076
+    // &heading 0-360
     NSURL *url = [NSURL URLWithString:s];
     NSURLRequest *req = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:req];
