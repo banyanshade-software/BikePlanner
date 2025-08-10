f4f7027b8df557b5edbff27e7053db46e984082e
Daniel <braun.daniel@free.fr>
2025-08-10 14:11:25 +0200

    refactor splitting AppDelegate

---
+++ BikePlanner/MapController.h
@@ -0,0 +1,16 @@
+//
+//  MapController.h
+//  BikePlanner
+//
+//  Created by Daniel on 10/08/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MapController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BikePlanner/MapController.m
@@ -0,0 +1,12 @@
+//
+//  MapController.m
+//  BikePlanner
+//
+//  Created by Daniel on 10/08/2025.
+//
+
+#import "MapController.h"
+
+@implementation MapController
+
+@end
