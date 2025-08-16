5ee35eb37f678e554825c95c12dbfd9195416daf
banyanshade-software <braun.daniel@free.fr>
2025-08-16 22:29:47 +0200

    add runloop monitor

--- BikePlanner/AppDelegate.m
+++ BikePlanner/AppDelegate.m
@@ -15,6 +15,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    [self installRunLoopLogger];
 }
 
 
@@ -35,4 +36,37 @@
     }
     return NO; // don’t
 }
+
+#pragma mark -
+
+static void RunLoopLogger(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info)
+{
+    NSString *phase;
+    switch (activity) {
+        case kCFRunLoopEntry: phase = @"Entry"; break;
+        case kCFRunLoopBeforeTimers: phase = @"BeforeTimers"; break;
+        case kCFRunLoopBeforeSources: phase = @"BeforeSources"; break;
+        case kCFRunLoopBeforeWaiting: phase = @"BeforeWaiting"; break;
+        case kCFRunLoopAfterWaiting: phase = @"AfterWaiting"; break;
+        case kCFRunLoopExit: phase = @"Exit"; break;
+        default: phase = [NSString stringWithFormat:@"Activity %lu", activity]; break;
+    }
+    NSLog(@"[RunLoop] %@", phase);
+}
+
+- (void)installRunLoopLogger
+{
+    CFRunLoopObserverContext context = {0, (__bridge void *)self, NULL, NULL, NULL};
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreate(
+        kCFAllocatorDefault,
+        kCFRunLoopAllActivities,
+        YES, // repeat
+        0,   // order
+        RunLoopLogger,
+        &context
+    );
+    CFRunLoopAddObserver(CFRunLoopGetMain(), observer, kCFRunLoopCommonModes);
+    CFRelease(observer);
+}
+
 @end
