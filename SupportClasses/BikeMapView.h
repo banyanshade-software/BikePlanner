70372e9ab376a7a67b1cbfdf2cfe751d08c80dac
banyanshade-software <braun.daniel@free.fr>
2025-08-17 22:09:54 +0200

    fix zoom button click on progress (disabled zoom control)

---
+++ SupportClasses/BikeMapView.h
@@ -0,0 +1,16 @@
+//
+//  BikeMapView.h
+//  BikePlanner
+//
+//  Created by Daniel Braun on 17/08/2025.
+//
+
+#import <MapKit/MapKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BikeMapView : MKMapView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SupportClasses/BikeMapView.m
@@ -0,0 +1,18 @@
+//
+//  BikeMapView.m
+//  BikePlanner
+//
+//  Created by Daniel Braun on 17/08/2025.
+//
+
+#import "BikeMapView.h"
+
+@implementation BikeMapView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
