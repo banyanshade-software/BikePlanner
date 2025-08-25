6dc8dd8406ec1606bb0a8f6a6d2425b39d1df9b1
banyanshade-software <braun.daniel@free.fr>
2025-08-25 20:44:20 +0200

    add accessory view to save panel

---
+++ BikePlanner/ExportAccessoryViewController.h
@@ -0,0 +1,17 @@
+//
+//  ExportAccessoryViewController.h
+//  BikePlanner
+//
+//  Created by Daniel Braun on 25/08/2025.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ExportAccessoryViewController : NSViewController
+@property (weak) IBOutlet NSButton *poiCheckbox;
+@property (assign, nonatomic) int outputFormatNum;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BikePlanner/ExportAccessoryViewController.m
@@ -0,0 +1,21 @@
+//
+//  ExportAccessoryViewController.m
+//  BikePlanner
+//
+//  Created by Daniel Braun on 25/08/2025.
+//
+
+#import "ExportAccessoryViewController.h"
+
+@interface ExportAccessoryViewController ()
+
+@end
+
+@implementation ExportAccessoryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
